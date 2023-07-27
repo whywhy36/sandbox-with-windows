09bf464f05b564b9c30dc1bb61abe403c6d351e6
Yuan Zhuang <whywhy36@gmail.com>
2023-07-27 21:06:10 +1200

    fix

--- solution/tf/env.sh
+++ solution/tf/env.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Generate an SSH key pair
-ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa_test -N "" -q
+ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N "" -q
 
 # Read the content of the public key file into a variable
-public_key=$(cat ~/.ssh/id_rsa_test.pub)
+public_key=$(cat ~/.ssh/id_rsa.pub)
 
 cat <<EOF
 {
     "sandbox_id": "$SANDBOX_ID",
-    "sandbox_name": "$SANDBOX_NAME"
+    "sandbox_name": "$SANDBOX_NAME",
     "ssh_pub": "$public_key"
 }
 EOF
