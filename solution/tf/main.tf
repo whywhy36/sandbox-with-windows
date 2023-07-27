184344415b8cefe36d9335c22324a8fc12a8942f
Yuan Zhuang <whywhy36@gmail.com>
2023-07-27 22:36:12 +1200

    update

--- solution/tf/main.tf
+++ solution/tf/main.tf
@@ -28,29 +28,29 @@ resource "aws_instance" "vm" {
     name = var.launch_template_name
   }
   get_password_data      = true
-  user_data = <<-EOT
-<powershell>
-# Install the OpenSSH Client
-Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+  user_data = << EOT
+    <powershell>
+    # Install the OpenSSH Client
+    Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
-# Install the OpenSSH Server
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    # Install the OpenSSH Server
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-# Start the sshd service
-Start-Service sshd
+    # Start the sshd service
+    Start-Service sshd
 
-Set-Service -Name sshd -StartupType 'Automatic'
+    Set-Service -Name sshd -StartupType 'Automatic'
 
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
+    # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+    if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+        Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+        New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+    } else {
+        Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+    }
 
-# Add the public key to the authrozied_keys file
-#Add-Content -Path "C:\ProgramData\ssh\administrators_authorized_keys" ${data.external.env.result.ssh_pub}
-</powershell>
+    # Add the public key to the authrozied_keys file
+    Add-Content -Path "C:\ProgramData\ssh\administrators_authorized_keys" ${data.external.env.result.ssh_pub}
+    </powershell>
 EOT
 }
