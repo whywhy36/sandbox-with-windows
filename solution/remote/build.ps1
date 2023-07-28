61dee739e70ca6543743573f2500211cf8f88195
Yuan Zhuang <whywhy36@gmail.com>
2023-07-28 13:55:34 +1200

    update

--- solution/remote/build.ps1
+++ solution/remote/build.ps1
@@ -1,7 +1,8 @@
 # Change the working directory to your project folder
 Set-Location -Path "C:\Users\Administrator\eShopOnWeb\src\Web"
 
-# Run the 'dotnet build' command
+dotnet dev-certs https
+dotnet dev-certs https --trust
 dotnet build
 
-Start-Job -ScriptBlock{dotnet run}
+Start-Job -ScriptBlock{dotnet run --launch-profile Web --urls "http://0.0.0.0:5000;https://0.0.0.0:5001"}
