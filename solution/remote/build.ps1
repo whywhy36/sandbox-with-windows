7fb5e8225fb17efb5638f9c437c9b643de3d9674
Yuan Zhuang <whywhy36@gmail.com>
2023-07-28 16:35:21 +1200

    update

--- solution/remote/build.ps1
+++ solution/remote/build.ps1
@@ -4,4 +4,4 @@ Set-Location -Path "C:\Users\Administrator\eShopOnWeb\src\Web"
 dotnet dev-certs https
 dotnet build
 $env:UseOnlyInMemoryDatabase = "true"
-Start-Process -FilePath "dotnet" -ArgumentList "run --launch-profile Web"
+Start-Process -FilePath "dotnet" -WorkingDirectory "C:\Users\Administrator\eShopOnWeb\src\Web" -ArgumentList "run --launch-profile Web" 
