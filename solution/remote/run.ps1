06e91513353167f7b27122c229407fed0cce9df7
Yuan Zhuang <whywhy36@gmail.com>
2023-07-31 11:07:56 +1200

    update

--- solution/remote/run.ps1
+++ solution/remote/run.ps1
@@ -1,3 +1,3 @@
 $env:UseOnlyInMemoryDatabase = "true"
-# dotnet dev-certs https
+dotnet dev-certs https
 Start-Process -FilePath "dotnet" -WorkingDirectory "C:\Users\Administrator\eShopOnWeb\src\Web" -ArgumentList "run --launch-profile Web" 
