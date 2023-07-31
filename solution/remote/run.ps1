84acbc57f9e8ea4dc3042866cf2b8f969231f88c
Yuan Zhuang <whywhy36@gmail.com>
2023-07-31 15:00:31 +1200

    update

--- solution/remote/run.ps1
+++ solution/remote/run.ps1
@@ -1,3 +1,2 @@
-Set-Location -Path "C:\Users\Administrator\eShopOnWeb\src\Web"
 $env:UseOnlyInMemoryDatabase = "true"
-dotnet run --launch-profile Web
+Start-Process -FilePath "dotnet" -WorkingDirectory "C:\Users\Administrator\eShopOnWeb\src\Web" -ArgumentList "run --launch-profile Web" 
