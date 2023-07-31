2bf565e268b72c9876db666a1e56a941c7962f32
Yuan Zhuang <whywhy36@gmail.com>
2023-07-31 12:25:38 +1200

    speed up

--- solution/remote/build.ps1
+++ solution/remote/build.ps1
@@ -1,6 +1,6 @@
 # Change the working directory to your project folder
 Set-Location -Path "C:\Users\Administrator\eShopOnWeb\src\Web"
-dotnet build
+#dotnet build
 
 dotnet dev-certs https -v
 dotnet dev-certs https -v
