512132239e46439be638f4432605aadef756f9b7
Yuan Zhuang <whywhy36@gmail.com>
2023-07-31 13:01:37 +1200

    update

--- solution/remote/build.ps1
+++ solution/remote/build.ps1
@@ -2,5 +2,4 @@
 Set-Location -Path "C:\Users\Administrator\eShopOnWeb\src\Web"
 dotnet build
 
-dotnet dev-certs https -ep "C:\Users\Administrator\dev-certs.pfx" -p 1234qwer
-dotnet dev-certs https --clean --import "C:\Users\Administrator\dev-certs.pfx" -p 1234qwer
+dotnet dev-certs https -v
