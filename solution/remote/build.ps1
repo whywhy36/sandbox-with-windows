a91ff24d9334a86baca2f42563d9af349c74dda0
Yuan Zhuang <whywhy36@gmail.com>
2023-07-31 12:36:43 +1200

    update

--- solution/remote/build.ps1
+++ solution/remote/build.ps1
@@ -1,6 +1,6 @@
 # Change the working directory to your project folder
 Set-Location -Path "C:\Users\Administrator\eShopOnWeb\src\Web"
-#dotnet build
+dotnet build
 
-dotnet dev-certs https -v
-dotnet dev-certs https -v
+dotnet dev-certs https -ep "C:\Users\Administrator\dev-certs.pfx" -p 1234qwer
+dotnet dev-certs https --clean --import "C:\Users\Administrator\dev-certs.pfx" -p 1234qwer
